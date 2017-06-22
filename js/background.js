f37deb2769d66dff6cd6b0e19ab50b7d7f9d6ea2
steelydylan <ess_president@me.com>
2017-06-23 00:35:56 +0900

    fix

--- js/background.js
+++ js/background.js
@@ -1,6 +1,6 @@
 async function get_pocket_count(url) {
   const pocketUrl = encodeURIComponent(`https://widgets.getpocket.com/v1/button?v=1&count=horizontal&url=${url}&src=${url}`);
-  const response = await fetch(`http://query.yahooapis.com/v1/public/yql?q=select * from xml where url="${pocketUrl}"&format=xml`)
+  const response = await fetch(`http://query.yahooapis.com/v1/public/yql?q=select * from xml where url%3D%22${pocketUrl}%22&format=xml`)
   const str = await response.text();
   const doc = await new window.DOMParser().parseFromString(str, "text/xml")
   return doc.querySelector('#cnt').innerHTML;
